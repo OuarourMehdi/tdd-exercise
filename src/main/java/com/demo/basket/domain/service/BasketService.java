edee6c48091e1974a2a2c6c87cba521b1fd61e95
Mehdi OUAROUR <mehdi_ouarour@carrefour.com>
2020-06-18 22:46:58 +0200

    Implement merge baskets with offers fusion

--- src/main/java/com/demo/basket/domain/service/BasketService.java
+++ src/main/java/com/demo/basket/domain/service/BasketService.java
@@ -3,10 +3,18 @@ package com.demo.basket.domain.service;
 import com.demo.basket.domain.exception.DomainException;
 import com.demo.basket.domain.model.Basket;
 import com.demo.basket.domain.model.KeepOption;
+import com.demo.basket.domain.model.Offer;
 import com.demo.basket.domain.spi.BasketPersistencePort;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Service
 public class BasketService {
@@ -14,7 +22,7 @@ public class BasketService {
     @Autowired
     private BasketPersistencePort persistencePort;
 
-    public Basket mergeBaskets(String customerId, String sessionId, KeepOption keep) {
+    public Basket mergeBaskets(String customerId, String sessionId, KeepOption keep, boolean mergeOffers) {
         Assert.hasText(customerId, "customerId must be not empty");
         Assert.hasText(sessionId, "customerId must be not empty");
         Assert.notNull(keep, "keepOption must be not null");
@@ -23,14 +31,41 @@ public class BasketService {
         switch (keep) {
             case AUTHENTICATED:
                 mergedBasket = getBasket(customerId);
+                if(mergeOffers) {
+                    Basket anonymousBasket = getBasket(sessionId);
+                    addNonExistentOfferToBasket(anonymousBasket.getOffers()).apply(mergedBasket);
+                }
                 break;
             case ANONYMOUS:
                 mergedBasket = getBasket(sessionId).toBuilder().customerId(customerId).build();
+                if(mergeOffers) {
+                    Basket authenticatedBasket = getBasket(customerId);
+                    addNonExistentOfferToBasket(authenticatedBasket.getOffers()).apply(mergedBasket);
+                }
+                
+
                 persistencePort.save(mergedBasket);
         }
 
         persistencePort.delete(sessionId);
         return mergedBasket;
+    }
+
+    private static Function<Basket, Basket> addNonExistentOfferToBasket(List<Offer> offersToAdd) {
+        return basket -> {
+            Set<String> existentOffersGtins = Optional.ofNullable(basket.getOffers())
+                    .map(existentOffers -> existentOffers.stream()
+                            .map(Offer::getGtin)
+                            .collect(Collectors.toSet()))
+                    .orElse(new HashSet<>());
+
+            offersToAdd.forEach(offerToAdd -> {
+                if(!existentOffersGtins.contains(offerToAdd.getGtin())) {
+                    basket.getOffers().add(offerToAdd);
+                }
+            });
+            return basket;
+        };
     }
 
     private Basket getBasket(String id) {
--- src/test/java/com/demo/basket/domain/service/BasketServiceTest.java
+++ src/test/java/com/demo/basket/domain/service/BasketServiceTest.java
@@ -1,11 +1,13 @@
 package com.demo.basket.domain.service;
 
 import com.demo.basket.domain.exception.DomainException;
-import com.demo.basket.domain.model.Offer;
 import com.demo.basket.domain.model.Basket;
 import com.demo.basket.domain.model.KeepOption;
+import com.demo.basket.domain.model.Offer;
 import com.demo.basket.domain.spi.BasketPersistencePort;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -16,7 +18,8 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Optional;
 import java.util.stream.Stream;
 
@@ -34,16 +37,215 @@ class BasketServiceTest {
     @InjectMocks
     BasketService basketService;
 
-    @Test
-    void mergeBasketsShouldThrowIAEWhenCustomerIdIsNull() {
-        assertThrows(IllegalArgumentException.class,
-                () -> basketService.mergeBaskets(null, null, KeepOption.AUTHENTICATED));
-    }
+    @Nested
+    @DisplayName("Merge AUTHENTICATED et ANONYMOUS baskets during connection tests")
+    class MergeBasketTest {
 
-    @Test
-    void mergeBasketsShouldThrowIAEWhenCustomerIdIsEmpty() {
-        assertThrows(IllegalArgumentException.class,
-                () -> basketService.mergeBaskets("", null, KeepOption.AUTHENTICATED));
+        @Test
+        void shouldThrowIAEWhenCustomerIdIsNull() {
+            assertThrows(IllegalArgumentException.class,
+                    () -> basketService.mergeBaskets(null, null, KeepOption.AUTHENTICATED, false));
+        }
+
+        @Test
+        void shouldThrowIAEWhenCustomerIdIsEmpty() {
+            assertThrows(IllegalArgumentException.class,
+                    () -> basketService.mergeBaskets("", null, KeepOption.AUTHENTICATED, false));
+        }
+
+        @ParameterizedTest(name = "{0}")
+        @MethodSource("com.demo.basket.domain.service.BasketServiceTest#invalidSessionIds")
+        void shouldThrowIAEWhenSessionIdIsInvalid(String scenarioName, String sessionId) {
+            assertThrows(IllegalArgumentException.class,
+                    () -> basketService.mergeBaskets("customerId", sessionId, KeepOption.AUTHENTICATED, false));
+        }
+
+        @Test
+        void shouldThrowIAEWhenKeepOptionIsNull() {
+            assertThrows(IllegalArgumentException.class,
+                    () -> basketService.mergeBaskets("customerId", "sessionId", null, false));
+        }
+
+        @Test
+        void shouldThrowDEWhenAuthenticatedBasketIsNotFound() {
+            String customerId = "customerId";
+            String sessionId = "sessionId";
+            when(persistencePort.find(customerId)).thenReturn(Optional.empty());
+
+            assertThrows(DomainException.class, () -> basketService.mergeBaskets(customerId, sessionId, KeepOption.AUTHENTICATED, false));
+        }
+
+        @Test
+        void shouldReturnAuthenticatedBasketWhenKeepOptionIsEqualToAUTHENTICATED() {
+            // Given
+            String customerId = "customerId";
+            String sessionId = "sessionId";
+            Basket authenticatedBasket = createBasket(customerId, "AUTHENTICATED-FSID",createOffer("123456", 2));
+            when(persistencePort.find(customerId)).thenReturn(Optional.of(authenticatedBasket));
+
+            // When
+            Basket mergedBasket = basketService.mergeBaskets(customerId, sessionId, KeepOption.AUTHENTICATED, false);
+
+            // Then
+            Basket expectedBasket = createBasket(customerId, "AUTHENTICATED-FSID",createOffer("123456", 2));
+            Assertions.assertThat(mergedBasket).isEqualTo(expectedBasket);
+        }
+
+        @Test
+        void shouldDeleteAnonymousBasketWhenKeepOptionIsEqualToAUTHENTICATED() {
+            // Given
+            String customerId = "customerId";
+            String sessionId = "sessionId";
+            Basket authenticatedBasket = createBasket(customerId, "AUTHENTICATED-FSID", createOffer("123456", 2));
+            when(persistencePort.find(customerId)).thenReturn(Optional.of(authenticatedBasket));
+
+            // When
+            basketService.mergeBaskets(customerId, sessionId, KeepOption.AUTHENTICATED, false);
+
+            // Then
+            Mockito.verify(persistencePort).delete(sessionId);
+        }
+
+        @Test
+        void shouldThrowDEWhenKeepOptionIsEqualToAUTHENTICATEDAndAnonymousBasketIsNotFound() {
+            String customerId = "customerId";
+            String sessionId = "sessionId";
+
+            when(persistencePort.find(sessionId)).thenReturn(Optional.empty());
+
+            assertThrows(DomainException.class, () -> basketService.mergeBaskets(customerId, sessionId, KeepOption.ANONYMOUS, false));
+        }
+
+        @Test
+        void shouldReplaceAuthenticatedBasketContentByAnonymousOneAndReturnResultWhenKeepOptionIsEqualToANONYMOUS() {
+            // Given
+            String customerId = "customerId";
+            String sessionId = "sessionId";
+            when(persistencePort.find(sessionId))
+                    .thenReturn(Optional.of(createBasket(sessionId, "ANONYMOUS-FSID", createOffer("563214", 3))));
+
+            // When
+            Basket mergedBasket = basketService.mergeBaskets(customerId, sessionId, KeepOption.ANONYMOUS, false);
+
+            // Then
+            Basket expectedBasket = createBasket(customerId, "ANONYMOUS-FSID", createOffer("563214", 3));
+            Assertions.assertThat(mergedBasket).isEqualTo(expectedBasket);
+        }
+
+        @Test
+        void shouldReplaceAuthenticatedBasketContentByAnonymousOneAndSaveAuthenticatedBasketResultWhenKeepOptionIsEqualToANONYMOUS() {
+            // Given
+            String customerId = "customerId";
+            String sessionId = "sessionId";
+            when(persistencePort.find(sessionId))
+                    .thenReturn(Optional.of(createBasket(sessionId, "ANONYMOUS-FSID", createOffer("563214", 3))));
+
+            // When
+            basketService.mergeBaskets(customerId, sessionId, KeepOption.ANONYMOUS, false);
+
+            // Then
+            Basket savedBasket = createBasket(customerId, "ANONYMOUS-FSID", createOffer("563214", 3));
+            verify(persistencePort).save(refEq(savedBasket));
+        }
+
+        @Test
+        void shouldDeleteAnonymousBasketWhenKeepOptionIsEqualToANONYMOUS() {
+            // Given
+            String customerId = "customerId";
+            String sessionId = "sessionId";
+            when(persistencePort.find(sessionId))
+                    .thenReturn(Optional.of(createBasket(sessionId, "ANONYMOUS-FSID", createOffer("563214", 3))));
+
+            // When
+            basketService.mergeBaskets(customerId, sessionId, KeepOption.ANONYMOUS, false);
+
+            // Then
+            Mockito.verify(persistencePort).delete(sessionId);
+        }
+
+        @Test
+        void shouldMergeOffersAndReturnAuthenticatedBasketWithMergedOffersWhenKeepOptionIsEqualToAUTHENTICATEDAndMergeOffersOptionIsTrueAndOffersAreDifferent() {
+            // Given
+            String customerId = "customerId";
+            String sessionId = "sessionId";
+            when(persistencePort.find(customerId))
+                    .thenReturn(Optional.of(createBasket(customerId, "AUTHENTICATED-FSID", createOffer("123456", 1))));
+            when(persistencePort.find(sessionId))
+                    .thenReturn(Optional.of(createBasket(sessionId, "ANONYMOUS-FSID", createOffer("563214", 3))));
+
+            // When
+            Basket mergedBasket = basketService.mergeBaskets(customerId, sessionId, KeepOption.AUTHENTICATED, true);
+
+            // Then
+            Basket expectedBasket = createBasket("customerId",
+                    "AUTHENTICATED-FSID",
+                    createOffer("123456", 1),
+                    createOffer("563214", 3));
+            Assertions.assertThat(mergedBasket).isEqualTo(expectedBasket);
+        }
+
+        @Test
+        void shouldMergeOffersAndReturnAuthenticatedBasketWithMergedOffersWhenKeepOptionIsEqualToAUTHENTICATEDAndMergeOffersOptionIsTrueAndSameOffers() {
+            // Given
+            String customerId = "customerId";
+            String sessionId = "sessionId";
+            when(persistencePort.find(customerId))
+                    .thenReturn(Optional.of(createBasket(customerId, "AUTHENTICATED-FSID", createOffer("123456", 1), createOffer("563214", 2))));
+            when(persistencePort.find(sessionId))
+                    .thenReturn(Optional.of(createBasket(sessionId, "ANONYMOUS-FSID", createOffer("563214", 3))));
+
+            // When
+            Basket mergedBasket = basketService.mergeBaskets(customerId, sessionId, KeepOption.AUTHENTICATED, true);
+
+            // Then
+            Basket expectedBasket = createBasket("customerId",
+                    "AUTHENTICATED-FSID",
+                    createOffer("123456", 1),
+                    createOffer("563214", 2));
+            Assertions.assertThat(mergedBasket).isEqualTo(expectedBasket);
+        }
+
+        @Test
+        void shouldMergeOffersAndReturnMergedBasketWhenKeepOptionIsEqualToANONYMOUSAndMergeOffersOptionIsTrue() {
+            // Given
+            String customerId = "customerId";
+            String sessionId = "sessionId";
+            when(persistencePort.find(customerId))
+                    .thenReturn(Optional.of(createBasket(customerId, "AUTHENTICATED-FSID", createOffer("123456", 1))));
+            when(persistencePort.find(sessionId))
+                    .thenReturn(Optional.of(createBasket(sessionId, "ANONYMOUS-FSID", createOffer("563214", 3))));
+
+            // When
+            Basket mergedBasket = basketService.mergeBaskets(customerId, sessionId, KeepOption.ANONYMOUS, true);
+
+            // Then
+            Basket expectedBasket = createBasket("customerId",
+                    "ANONYMOUS-FSID",
+                    createOffer("563214", 3),
+                    createOffer("123456", 1));
+            Assertions.assertThat(mergedBasket).isEqualTo(expectedBasket);
+        }
+
+        @Test
+        void shouldMergeOffersAndReturnMergedBasketWhenKeepOptionIsEqualToANONYMOUSAndMergeOffersOptionIsTrueAndSameOffers() {
+            // Given
+            String customerId = "customerId";
+            String sessionId = "sessionId";
+            when(persistencePort.find(customerId))
+                    .thenReturn(Optional.of(createBasket(customerId, "AUTHENTICATED-FSID", createOffer("123456", 1))));
+            when(persistencePort.find(sessionId))
+                    .thenReturn(Optional.of(createBasket(sessionId, "ANONYMOUS-FSID", createOffer("123456", 5), createOffer("563214", 3))));
+
+            // When
+            Basket mergedBasket = basketService.mergeBaskets(customerId, sessionId, KeepOption.ANONYMOUS, true);
+
+            // Then
+            Basket expectedBasket = createBasket("customerId",
+                    "ANONYMOUS-FSID",
+                    createOffer("123456", 5),
+                    createOffer("563214", 3));
+            Assertions.assertThat(mergedBasket).isEqualTo(expectedBasket);
+        }
     }
 
     static Stream<Arguments> invalidSessionIds() {
@@ -53,123 +255,16 @@ class BasketServiceTest {
         );
     }
 
-    @ParameterizedTest(name = "{0}")
-    @MethodSource("invalidSessionIds")
-    void mergeBasketsShouldThrowIAEWhenSessionIdIsInvalid(String scenarioName, String sessionId) {
-        assertThrows(IllegalArgumentException.class,
-                () -> basketService.mergeBaskets("customerId", sessionId, KeepOption.AUTHENTICATED));
-    }
-
-    @Test
-    void mergeBasketsShouldThrowIAEWhenKeepOptionIsNull() {
-        assertThrows(IllegalArgumentException.class,
-                () -> basketService.mergeBaskets("customerId", "sessionId", null));
-    }
-
-    @Test
-    void mergeBasketsShouldThrowDEWhenAuthenticatedBasketIsNotFound() {
-        String customerId = "customerId";
-        String sessionId = "sessionId";
-        when(persistencePort.find(customerId)).thenReturn(Optional.empty());
-
-        assertThrows(DomainException.class, () -> basketService.mergeBaskets(customerId, sessionId, KeepOption.AUTHENTICATED));
-    }
-
-
-    @Test
-    void mergeBasketsShouldReturnAuthenticatedBasketWhenKeepOptionIsEqualToAUTHENTICATED() {
-        // Given
-        String customerId = "customerId";
-        String sessionId = "sessionId";
-        Basket authenticatedBasket = createBasket(customerId, "AUTHENTICATED-FSID","123456", 2);
-        when(persistencePort.find(customerId)).thenReturn(Optional.of(authenticatedBasket));
-
-        // When
-        Basket mergedBasket = basketService.mergeBaskets(customerId, sessionId, KeepOption.AUTHENTICATED);
-
-        // Then
-        Basket expectedBasket = createBasket(customerId, "AUTHENTICATED-FSID","123456", 2);
-        Assertions.assertThat(mergedBasket).isEqualTo(expectedBasket);
-    }
-
-    @Test
-    void mergeBasketsShouldDeleteAnonymousBasketWhenKeepOptionIsEqualToAUTHENTICATED() {
-        // Given
-        String customerId = "customerId";
-        String sessionId = "sessionId";
-        Basket authenticatedBasket = createBasket(customerId, "AUTHENTICATED-FSID", "123456", 2);
-        when(persistencePort.find(customerId)).thenReturn(Optional.of(authenticatedBasket));
-
-        // When
-        basketService.mergeBaskets(customerId, sessionId, KeepOption.AUTHENTICATED);
-
-        // Then
-        Mockito.verify(persistencePort).delete(sessionId);
-    }
-
-    @Test
-    void mergeBasketsShouldThrowDEWhenKeepOptionIsEqualToAUTHENTICATEDAndAnonymousBasketIsNotFound() {
-        String customerId = "customerId";
-        String sessionId = "sessionId";
-
-        when(persistencePort.find(sessionId)).thenReturn(Optional.empty());
-
-        assertThrows(DomainException.class, () -> basketService.mergeBaskets(customerId, sessionId, KeepOption.ANONYMOUS));
-    }
-
-    @Test
-    void mergeBasketsShouldReplaceAuthenticatedBasketContentByAnonymousOneAndReturnResultWhenKeepOptionIsEqualToANONYMOUS() {
-        // Given
-        String customerId = "customerId";
-        String sessionId = "sessionId";
-        when(persistencePort.find(sessionId))
-                .thenReturn(Optional.of(createBasket(sessionId, "ANONYMOUS-FSID", "563214", 3)));
-
-        // When
-        Basket mergedBasket = basketService.mergeBaskets(customerId, sessionId, KeepOption.ANONYMOUS);
-
-        // Then
-        Basket expectedBasket = createBasket(customerId, "ANONYMOUS-FSID", "563214", 3);
-        Assertions.assertThat(mergedBasket).isEqualTo(expectedBasket);
-    }
-
-    @Test
-    void mergeBasketsShouldReplaceAuthenticatedBasketContentByAnonymousOneAndSaveAuthenticatedBasketResultWhenKeepOptionIsEqualToANONYMOUS() {
-        // Given
-        String customerId = "customerId";
-        String sessionId = "sessionId";
-        when(persistencePort.find(sessionId))
-                .thenReturn(Optional.of(createBasket(sessionId, "ANONYMOUS-FSID", "563214", 3)));
-
-        // When
-        basketService.mergeBaskets(customerId, sessionId, KeepOption.ANONYMOUS);
-
-        // Then
-        Basket savedBasket = createBasket(customerId, "ANONYMOUS-FSID", "563214", 3);
-        verify(persistencePort).save(refEq(savedBasket));
-    }
-
-    @Test
-    void mergeBasketsShouldDeleteAnonymousBasketWhenKeepOptionIsEqualToANONYMOUS() {
-        // Given
-        String customerId = "customerId";
-        String sessionId = "sessionId";
-        when(persistencePort.find(sessionId))
-                .thenReturn(Optional.of(createBasket(sessionId, "ANONYMOUS-FSID", "563214", 3)));
-
-        // When
-        basketService.mergeBaskets(customerId, sessionId, KeepOption.ANONYMOUS);
-
-        // Then
-        Mockito.verify(persistencePort).delete(sessionId);
-    }
-
-    private Basket createBasket(String customerId, String facilityServiceId, String offerGtin, int offerQuantity) {
+    static Basket createBasket(String customerId, String facilityServiceId, Offer... offers) {
         return Basket.builder()
                 .customerId(customerId)
                 .facilityServiceId(facilityServiceId)
-                .offers(Collections.singletonList(Offer.builder().gtin(offerGtin).quantity(offerQuantity).build()))
+                .offers(new ArrayList<>(Arrays.asList(offers)))
                 .build();
+    }
+
+    static Offer createOffer(String gtin, long quantity) {
+        return Offer.builder().gtin(gtin).quantity(quantity).build();
     }
 
 }
